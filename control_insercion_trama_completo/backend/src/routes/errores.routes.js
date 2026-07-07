6e8bbdbb5e0087624a7712a4e50c5b30bb60c88b
Manfredo_Juan <manfredojuanignacio08@gmail.com>
2026-07-07 01:36:17 -0300

    Delete control_insercion_trama_completo directory

--- control_insercion_trama_completo/backend/src/routes/errores.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as erroresController from '../controllers/errores.controller.js';
-
-const router = express.Router();
-
-router.get('/', erroresController.listarErrores);
-router.post('/', erroresController.crearError);
-
-export default router;
