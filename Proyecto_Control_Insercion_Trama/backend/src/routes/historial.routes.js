43e22b31fbdf96e286a21166e8b0e4b6ded2fbc5
Manfredo_Juan <manfredojuanignacio08@gmail.com>
2026-07-07 01:35:56 -0300

    Delete Proyecto_Control_Insercion_Trama directory

--- Proyecto_Control_Insercion_Trama/backend/src/routes/historial.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as historialController from '../controllers/historial.controller.js';
-
-const router = express.Router();
-
-router.get('/', historialController.listarHistorialGlobal);
-
-export default router;
